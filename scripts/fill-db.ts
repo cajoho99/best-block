e8878366a5e7d1a893eb20c460003376dbe0afa1
Carl Holmberg <holmberg.carl.j@gmail.com>
2023-01-03 15:52:15 +0100

    image file downloader and script updated to use other api

--- scripts/fill-db.ts
+++ scripts/fill-db.ts
@@ -1,41 +1,43 @@
-import axios from 'axios';
-import cheerio from 'cheerio';
+import { promises as fs } from "fs";
 
+const downloadImage = async (url: string, path: string) => {
+    const response = await fetch(url);
+    const blob = await response.blob();
+    const arrayBuffer = await blob.arrayBuffer();
+    const buffer = Buffer.from(arrayBuffer);
+    await fs.writeFile(path, buffer);
+}
 import { PrismaClient } from '@prisma/client'
 
+const fileExists = async (path:string ) => !!(await fs.stat(path).catch(e => false));
 
 const prisma = new PrismaClient()
 
 export class BlockScraper {
     async scrapeBlocks(url: string) {
-        const response = await axios.get(url);
-        const html = response.data;
-
-        const $ = cheerio.load(html);
-
-        const blockData = $('.wikia-gallery-item');
-
+        const response = await fetch(url);
+        const blocks = await response.json();
         const outputData = []
 
-        for (let i = 0; i < blockData.length; i++) {
-            const name = $(blockData[i]).find(".lightbox-caption").text();
-            if(name == "" || name === undefined) continue;
-            const imageName = $(blockData[i]).find("img").attr("data-src")
-            if(imageName == "" || imageName === undefined) continue;
-           
-            outputData.push({
-                name,
-                imageURL: imageName
-            })
-        }
+        for (const block of blocks) {
 
+            if (!await fileExists('./public/blocks/' + block.namespacedId + '.png')) {
+                await downloadImage(block.image, './public/blocks/' + block.namespacedId + '.png')                
+                console.log(block.name + "was downloaded" )
+            }
+            outputData.push({
+                name: block.name,
+                imageURL: '/blocks/' + block.namespacedId + '.png'
+            })
+
+        }
         return outputData
     }
 }
 
 async function main() {
     const scraper = new BlockScraper();
-    const data = await scraper.scrapeBlocks("https://minecraft-archive.fandom.com/wiki/Blocks/Gallery");
+    const data = await scraper.scrapeBlocks("https://minecraft-api.vercel.app/api/blocks");
     console.log(data)
     const creation = await prisma.block.createMany({
         data: data 
