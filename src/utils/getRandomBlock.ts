9c71dd5324c12f1e2197a9f3a7a55c96952b4bed
Carl Holmberg <holmberg.carl.j@gmail.com>
2023-02-24 22:35:02 +0100

    Some styling for the index page

---
+++ src/utils/getRandomBlock.ts
@@ -0,0 +1,17 @@
+const NO_BLOCKS = 893
+
+export const getRandomBlock: (exclude: number) => number = (exclude) => {
+
+    const index = Math.floor(Math.random() * NO_BLOCKS) + 1
+
+    if(index !== exclude) return index;
+
+    return getRandomBlock(exclude)
+} 
+
+export const getRandomPair = () => {
+    const firstId = getRandomBlock();
+    const secondId = getRandomBlock(firstId);
+
+    return [firstId, secondId]
+}
